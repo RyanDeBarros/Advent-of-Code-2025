ef37097e4e3a17deb152f9826020aa3c4408b870
RyanDeBarros <ryan2004db@gmail.com>
2025-12-07 13:04:03 -0500

    Day 6

---
+++ Day 6/P1.py
@@ -0,0 +1,46 @@
+lines = []
+with open("Input.txt") as f:
+	rows = sum(1 for _ in f) - 1
+	f.seek(0)
+	for _ in range(rows):
+		lines.append(f.readline().rstrip())
+	operations = f.readline().rstrip()
+
+
+class Expression:
+	def __init__(self, rows):
+		self.nums = ["" for _ in range(rows)]
+		self.op = ""
+
+	def eval(self):
+		if self.op.strip() == '*':
+			val = 1
+			for num in self.nums:
+				val *= int(num.strip())
+			return val
+		elif self.op.strip() == '+':
+			val = 0
+			for num in self.nums:
+				val += int(num.strip())
+			return val
+		else:
+			return 0
+
+
+line_width = max(max(len(line) for line in lines), len(operations)) + 1
+for i in range(rows):
+	lines[i] = lines[i].ljust(line_width)
+operations = operations.ljust(line_width)
+
+expressions = []
+expression = Expression(rows)
+for cs in zip(*lines, operations):
+	if all(c == ' ' for c in cs):
+		expressions.append(expression)
+		expression = Expression(rows)
+	else:
+		for i, n in enumerate(cs[:-1]):
+			expression.nums[i] += n
+		expression.op += cs[-1]
+
+print(sum(exp.eval() for exp in expressions))
